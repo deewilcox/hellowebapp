e7f961d3c8e8eb3d334befff7b9a23c01f8fe9aa
Dee Wilcox <dee.wilcox@mac.com>
2016-02-08 22:37:24 -0600

    Building admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+# import model
+from collection.models import Service
+
+# set up automated slug creation
+class ServiceAdmin(admin.ModelAdmin):
+    model = Service
+    list_display = ('name', 'description',)
+    prepopulated_fields = {'slug':('name',)}
+
+# register model
+admin.site.register(Service, ServiceAdmin)
+
+
+
