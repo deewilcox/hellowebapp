316c971d4cf723c8a346e96ec854792b227f06e7
Dee Wilcox <dee.wilcox@mac.com>
2016-02-14 09:14:37 -0600

    Modify views

--- services/views.py
+++ services/views.py
@@ -24,25 +24,28 @@ def normalize_data(data):
     normalized_data = json_normalize(data,'regions',['region','instanceType'])
     return normalized_data
 
-
-spot = load_data(url1)
-data = spot['config']['regions']
-print normalize_data(data)
-
 # Create your views here.
 def index(request):
     return render(request, 'index.html',{
-        'greeting':'Welcome to HelloWebApp, the AWS Services Edition',
+        'greeting':'View Current Pricing for AWS EC2 and Spot Instances',
     })
 
 def services(request):
     url1 = 'http://spot-price.s3.amazonaws.com/spot.js'
     url2 = 'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
+
     spot = load_data(url1)
-    data = spot['config']['regions']
-    spot_data =  normalize_data(data)
+    data1 = spot['config']['regions']
+    spot_data =  normalize_data(data1)
+
+    #ec2 = load_data(url2)
+    #data2 = ec2['config']['regions']
+    #ec2_data = normalize_data(data2)
+
     return render(request, 'index.html',{
-        'spot':spot_data,
-        'heading':'This is normalized data for AWS Spot instances',
+        'spot_data':spot_data,
+        'spot_heading':'This is normalized data for AWS Spot instance pricing',
+        'ec2_data':'',
+        'ec2_heading':'This is normalized data for AWS EC2 instance pricing'
     })
 
