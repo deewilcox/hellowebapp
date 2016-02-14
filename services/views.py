d8823aadada2f74a3ba3e95251f7c39ea128fbb8
Dee Wilcox <dee.wilcox@mac.com>
2016-02-14 17:26:55 -0600

    Adding notes to failed attempt at using json_normalize() with pandas

--- services/views.py
+++ services/views.py
@@ -4,27 +4,40 @@ import re
 import json
 from pandas.io.json import json_normalize
 
+# Define functions specific to the application
+
 """
 The supplied API endpoints are JS callbacks.
 So, we need to clean them up so we can parse the file.
 This function takes in the API endpoint URL, cleans up the JS callback, parses the JSON, and returns a normalized JSON object.
 """    
+
+define clean_output(json_string):
+    modified_json_string = re.sub(re.compile(r'/\*.*\*/\n', re.DOTALL), '', json_string)
+    modified_json_string = re.sub(r'^callback\(', '', modified_json_string)
+    modified_json_string = re.sub(r'\);*$', '', modified_json_string)
+    return modified_json_string
+
 def load_data(url):
-    output = urllib2.urlopen(url)
-    request = output.read()
-
-    modified_request = re.sub(re.compile(r'/\*.*\*/\n', re.DOTALL), '', request)
-    modified_request = re.sub(r'^callback\(', '', modified_request)
-    modified_request = re.sub(r'\);*$', '', modified_request)
-
+    output = requests.get(url)
+    # Not using requests.json() here because the API does not hand back clean json
+    request = output.text
+    modified_request = clean_output(request)
     obj = json.loads(modified_request)
     return obj
 
+""" 
+This funciton is not in use, but keeping to revisit at a later time.
+Initially, I planned to use json_normalize and pandas to plot the data. 
+However, I ran into an error getting beyond json_normalize(data,['instanceTypes',['sizes']],'region') and into the pricing data.
+It will be less efficient, but will need to iterate over the data and built the dictionary that I need.
+"""
+
 def normalize_data(data):
-    normalized_data = json_normalize(data,'regions',['region','instanceType'])
+    normalized_data = json_normalize(data,['instanceTypes',['sizes']],'region')
     return normalized_data
 
-# Create your views here.
+# Define functions specific to loading views
 def index(request):
     return render(request, 'index.html',{
         'greeting':'View Current Pricing for AWS EC2 and Spot Instances',
