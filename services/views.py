b8b65a632b99ce1104d1983b5bb67a3c9f2ff0e8
Dee Wilcox <dee.wilcox@mac.com>
2016-02-14 22:26:38 -0600

    New functions for parsing and charting data

--- services/views.py
+++ services/views.py
@@ -18,19 +18,73 @@ define clean_output(json_string):
     modified_json_string = re.sub(r'\);*$', '', modified_json_string)
     return modified_json_string
 
-def load_data(url):
+def load_data(url,service_type):
     output = requests.get(url)
     # Not using requests.json() here because the API does not hand back clean json
     request = output.text
     modified_request = clean_output(request)
-    obj = json.loads(modified_request)
+
+    if service_type == 'spot':
+        obj = json.loads(modified_request)
+    elif service_type == 'ec2':
+        obj = demjson.decode(modified_request)
+    else:
+        obj = modified_request
+    
     return obj
 
+def prepare_instance_data(data):
+    # Initialize empty chart_data dictionary. 
+    chart_data = {} 
+    instance_types = data[0]['instanceTypes'][0]
+
+    # Parse through data and build spread of prices by region, instance type, size, and price
+    for instance in data:
+        if 'region' in instance and instance['region']:
+            region = instance['region']
+
+        for instance_type in instance['instanceTypes']:
+            type_name = instance_type[u'type']
+            
+            for sizes in instance_type['sizes']:
+                size = sizes[u'size']
+                if 'valueColumns' in sizes and sizes['valueColumns']:
+                    for columns in sizes['valueColumns']:
+                        name = columns[u'name']
+                        label_name = region + '_' + type_name + '_' + name + '_' + size
+                        price = columns[u'prices'][u'USD']
+                        if price:
+                            chart_data[label_name]=round(float(price),2)
+
+    return chart_data 
+
+def chart_instance(chart_data):
+    labels = []
+    values = []
+     
+    for instance_label, price in chart_data:
+        labels.insert(0, instance_label)
+        values.insert(0, price)
+        
+    ind = np.arange(len(values))
+    fig = plt.figure(figsize=(len(labels) * 1.8, 10))
+
+    ax = fig.add_subplot(1, 1, 1)
+    ax.bar(ind, values, 0.3, align='center')
+
+    plt.ylabel('Instance Prices')
+    plt.xlabel('Instance Region, Size, and Type')
+    ax.set_xticks(ind + 0.3)
+    ax.set_xticklabels(labels)
+    plt.grid(True)
+    plt.show()
+    return plt
+
 """ 
-This funciton is not in use, but keeping to revisit at a later time.
+This function is not in use, but keeping to revisit at a later time.
 Initially, I planned to use json_normalize and pandas to plot the data. 
 However, I ran into an error getting beyond json_normalize(data,['instanceTypes',['sizes']],'region') and into the pricing data.
-It will be less efficient, but will need to iterate over the data and built the dictionary that I need.
+It will be less efficient, but I will instead iterate over the data and built the dictionary that I need.
 """
 
 def normalize_data(data):
@@ -47,18 +101,18 @@ def services(request):
     url1 = 'http://spot-price.s3.amazonaws.com/spot.js'
     url2 = 'http://a0.awsstatic.com/pricing/1/ec2/linux-od.min.js'
 
-    spot = load_data(url1)
+    spot = load_data(url1,'spot')
     data1 = spot['config']['regions']
     spot_data =  normalize_data(data1)
 
-    #ec2 = load_data(url2)
+    #ec2 = load_data(url2,'ec2')
     #data2 = ec2['config']['regions']
     #ec2_data = normalize_data(data2)
 
     return render(request, 'index.html',{
         'spot_data':spot_data,
-        'spot_heading':'This is normalized data for AWS Spot instance pricing',
+        'spot_heading':'This is a data visualization for AWS Spot instance pricing',
         'ec2_data':'',
-        'ec2_heading':'This is normalized data for AWS EC2 instance pricing'
+        'ec2_heading':'This is a data visualization for AWS EC2 instance pricing'
     })
 
